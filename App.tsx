375ef79fb825fb012c75703c98d1d0529a9be3c7
lorenasg1 <lorenasg1@gmail.com>
2021-08-22 17:53:55 -0300

    Challenge completed

--- App.tsx
+++ App.tsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
+import { DMSans_400Regular, DMSans_700Bold } from '@expo-google-fonts/dm-sans';
 import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font';
-import { DMSans_400Regular, DMSans_700Bold } from '@expo-google-fonts/dm-sans';
-
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { ThemeProvider } from 'styled-components/native';
 import { AuthProvider } from './src/hooks/useAuth';
 import { Routes } from './src/routes';
-import { ThemeProvider } from 'styled-components/native';
-
+import { Home } from './src/screens/Home';
 import theme from './src/styles/theme';
 
 export default function App() {
   const [isLoaded] = useFonts({
     DMSans_400Regular,
-    DMSans_700Bold
-  })
-  
+    DMSans_700Bold,
+  });
+
   if (!isLoaded) {
-    return <AppLoading />
+    return <AppLoading />;
   }
 
   return (
     <AuthProvider>
       <ThemeProvider theme={theme}>
         <StatusBar style="light" backgroundColor="transparent" translucent />
+        {/* <Home /> */}
         <Routes />
       </ThemeProvider>
     </AuthProvider>
